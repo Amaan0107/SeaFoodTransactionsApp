dcd265db58db54b7d8cd89c451dda80df90b325f
Amaan <adilawar2007@gmail.com>
2025-10-13 12:59:09 -0700

    Lets see test

---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,48 @@
+package com.pluralsight;
+
+public class Transaction {
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private Double amount;
+
+    public Transaction(String date, String time, String description, String vendor, Double amount){
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "date='" + date + '\'' +
+                ", time='" + time + '\'' +
+                ", description='" + description + '\'' +
+                ", vendor='" + vendor + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+}
